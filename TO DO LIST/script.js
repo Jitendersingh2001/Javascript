45f3642a9d9a6b8f8076e96e39837cdcb421acce
Jitender singh <jitendersinghjatt09@gmail.com>
2023-07-07 21:23:26 +0530

    to do list done

--- TO DO LIST/script.js
+++ TO DO LIST/script.js
@@ -3,6 +3,7 @@
 let addbtn = document.getElementById("push");
 let taskcontainer = document.getElementById("tasks");
 let inputtext = document.querySelector('input[type="text"]');
+let i = 0;
 /*ADD TASK */
 addbtn.addEventListener("click", addTask);
 inputtext.addEventListener("keydown", (e) => {
@@ -11,23 +12,49 @@ inputtext.addEventListener("keydown", (e) => {
   }
 });
 function addTask() {
-  if (inputtext.value.trim() !== "") {
-    let task = ` <div class="task">
-    <span id="taskname">${inputtext.value}</span>
-    <div class="bottons">
-      <button class="DONE">
-        <i class="fa fa-check"></i>
-      </button>
-      <button class="delete">
-        <i class="far fa-trash-alt"></i>
-      </button>
-    </div>
-  </div>`;
-    taskcontainer.insertAdjacentHTML("afterbegin", task);
-    inputtext.value = "";
-  } else {
-    alert("PLEASE ENTER TASK");
+  if (inputtext.value.length == 0) alert("PLEASE ENTER THE TASK");
+  else {
+    if (i === 10) alert("PLEASE COMPLETE PREVIOUS TASK !!");
+    else {
+      let task = ` <div class="task">
+      <span id="taskname">${inputtext.value}</span>
+      <div class="bottons">
+        <button id="DONE">
+          <i class="fa fa-check"></i>
+        </button>
+        <button id="delete">
+          <i class="far fa-trash-alt"></i>
+        </button>
+      </div>
+    </div>`;
+      taskcontainer.insertAdjacentHTML("afterbegin", task);
+      inputtext.value = "";
+      i++;
+      inputtext.placeholder = "ADD TASK";
+      let deleteBtns = document.querySelectorAll("#delete");
+      let doneBtns = document.querySelectorAll("#DONE");
+
+      deleteBtns.forEach((btn) => {
+        btn.addEventListener("click", deleteTask);
+      });
+      doneBtns.forEach((btn) => {
+        btn.addEventListener("click", doneTask);
+      });
+    }
   }
-  inputtext.placeholder = "ADD TASK";
 }
-/*DELETE TASK*/
+/* DELETE TASK */
+function deleteTask() {
+  let task = this.closest(".task");
+  task.remove();
+  i--;
+}
+/* DONE TASK */
+function doneTask() {
+  let task = this.closest(".task");
+  let taskname = task.querySelector("#taskname");
+  taskname.style.color = "#808080";
+  taskname.style.textDecoration = "line-through";
+  taskname.textContent += " (DONE) ";
+  i--;
+}
